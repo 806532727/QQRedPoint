333b72691caeccd0545f2fd36007aab50f97774a
806532727 <39822614+806532727@users.noreply.github.com>
2019-05-21 08:11:24 +0800

    QQ红点

---
+++ app/src/main/java/www/lenovo/com/qqredpoint/MainActivity.java
@@ -0,0 +1,52 @@
+package www.lenovo.com.qqredpoint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final FrameLayout containerView = (FrameLayout) findViewById(R.id.container);
+
+        final FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
+
+        findViewById(R.id.btn1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RedPointView redPointView = new RedPointView(MainActivity.this);
+                redPointView.setLayoutParams(layoutParams);
+                containerView.removeAllViews();
+                containerView.addView(redPointView);
+            }
+        });
+
+        findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RedPointViewCopy redPointView = new RedPointViewCopy(MainActivity.this);
+                redPointView.setLayoutParams(layoutParams);
+                containerView.removeAllViews();
+                containerView.addView(redPointView);
+            }
+        });
+
+        findViewById(R.id.btn3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RedPointControlView redPointView = new RedPointControlView(MainActivity.this);
+                redPointView.setLayoutParams(layoutParams);
+                containerView.removeAllViews();
+                containerView.addView(redPointView);
+            }
+        });
+    }
+}
